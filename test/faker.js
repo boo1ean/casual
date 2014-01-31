dccc76e1f146a6d5c31aef592a736668b9b16147
boo1ean <boo1ean0807@gmail.com>
2014-01-31 18:57:55 +0200

    Add common generators test

---
+++ test/faker.js
@@ -0,0 +1,21 @@
+var faker = require('../');
+
+describe('Faker API', function() {
+	var test = function(name) {
+		var first = faker[name];
+		var second = faker[name];
+		first.should.not.be.equal(second);
+	};
+
+	describe('Common generators', function() {
+		it('Should work pretty well', function() {
+			test('name');
+			test('firstname');
+			test('lastname');
+			test('domain');
+			test('sentence');
+			test('title');
+			test('email');
+		});
+	});
+});
