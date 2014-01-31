3147063b38d3cc525d2f8e370d4155ed6b908e73
boo1ean <boo1ean0807@gmail.com>
2014-02-01 01:50:49 +0200

    Improve double generator

--- src/generators/double.js
+++ src/generators/double.js
@@ -2,5 +2,5 @@ module.exports = function(from, to) {
 	from = from || Number.MIN_VALUE;
 	to   = to || Number.MAX_VALUE;
 
-	return Math.random() * to + from;
+	return from + (to - from) * Math.random();
 };
