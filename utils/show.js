3d41f39f074525c216e9558373fbb67231cd3b65
boo1ean <boo1ean0807@gmail.com>
2014-02-02 01:23:53 +0200

    Improve show util

--- utils/show.js
+++ utils/show.js
@@ -1,14 +1,45 @@
 var table = require('text-table');
 
-var provider = process.argv.pop();
-var provider = require('../src/providers/' + provider);
+var providers = [
+	'address',
+	'color',
+	'date',
+	'internet',
+	'misc',
+	'number',
+	'payment',
+	'person',
+	'text'
+];
 
-var result = [];
-for (var generator in provider) {
-	result.push([
-		generator,
-		typeof provider[generator] === 'function' ? provider[generator]() : provider.generator()
-	]);
+var render_table = function(provider_name) {
+	provider = require('../src/providers/' + provider_name);
+
+	var result = [];
+	for (var generator in provider) {
+		result.push([
+			generator,
+			typeof provider[generator] === 'function' ? provider[generator]() : provider.generator()
+		]);
+	};
+
+	console.log('\nProvider:', provider_name, '\n');
+	console.log(table(result));
 };
 
-console.log(table(result));
+var provider = process.argv.pop();
+
+if (provider === __filename) {
+
+	providers.forEach(render_table);
+
+} else if (providers.indexOf(provider) === -1) {
+
+	console.log('Please use correct provider name:', providers.join(', '));
+	process.exit();
+
+} else {
+
+	render_table(provider);
+
+}
