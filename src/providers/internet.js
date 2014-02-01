222d46fe65edc832f3b1cfe5f3dfe4277f9ff73d
boo1ean <boo1ean0807@gmail.com>
2014-02-02 00:46:15 +0200

    Drop faker from internet provider

--- src/providers/internet.js
+++ src/providers/internet.js
@@ -1,18 +1,28 @@
-var faker = require('Faker');
-module.exports = {
+var person = require('./person'),
+    integer = require('./number').integer,
+    helpers = require('../helpers');
+
+var random_element = helpers.random_element;
+
+var domain_suffix = ['co.uk', 'com', 'us', 'net', 'ca', 'biz', 'info', 'name', 'io', 'org', 'biz', 'tv', 'me'];
+
+var provider = {
 	domain: function() {
-		return faker.Internet.domainName();
+		var domain_name = random_element([person.first_name(), person.last_name()]).toLowerCase();
+		return domain_name + '.' + random_element(domain_suffix);
 	},
 
 	email: function() {
-		return faker.Internet.email()
+		return person.username().toLowerCase() + '@' + provider.domain();
 	},
 
 	url: function() {
-		return faker.Internet.domainName();
+		return provider.domain();
 	},
 
 	ip: function() {
-		return faker.Internet.ip();
+		return [integer(0, 255), integer(0, 255), integer(0, 255), integer(0, 255)].join('.');
 	}
-}
+};
+
+module.exports = provider;
