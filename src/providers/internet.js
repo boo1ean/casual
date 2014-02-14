554e0eadd691495850a28b5778be28e0e363f386
boo1ean <boo1ean0807@gmail.com>
2014-02-15 00:40:32 +0200

    Rewrite internet provider with populate appoach, add some generators

--- src/providers/internet.js
+++ src/providers/internet.js
@@ -1,17 +1,52 @@
 var provider = {
-    domain_suffix: ['co.uk', 'com', 'us', 'net', 'ca', 'biz', 'info', 'name', 'io', 'org', 'biz', 'tv', 'me'],
+	top_level_domains: ['co.uk', 'com', 'us', 'net', 'ca', 'biz', 'info', 'name', 'io', 'org', 'biz', 'tv', 'me'],
+
+	free_email_domains: ['gmail.com', 'yahoo.com', 'hotmail.com'],
+
+	user_name_formats: [
+		'{{last_name}}.{{first_name}}',
+		'{{first_name}}.{{last_name}}',
+		'{{first_name}}_{{last_name}}',
+		'{{last_name}}_{{first_name}}'
+	],
+
+	email_formats: [
+		'{{user_name}}@{{domain}}',
+		'{{user_name}}@{{free_email_domain}}'
+	],
+
+	url_formats: [
+		'http://www.{{domain}}/',
+		'http://{{domain}}/'
+	],
+
+	domain_formats: [
+		'{{first_name}}.{{top_level_domain}}',
+		'{{last_name}}.{{top_level_domain}}'
+	],
+
+	free_email_domain: function() {
+		return this.random_element(this.free_email_domains);
+	},
+
+	top_level_domain: function() {
+		return this.random_element(this.top_level_domains);
+	},
+
+	user_name: function() {
+		return this.populate_one_of(this.user_name_formats);
+	},
 
 	domain: function() {
-		var domain_name = this.random_element([this.first_name, this.last_name]).toLowerCase();
-		return domain_name + '.' + this.random_element(this.domain_suffix);
+		return this.populate_one_of(this.domain_formats);
 	},
 
 	email: function() {
-		return this.username.toLowerCase() + '@' + this.domain;
+		return this.populate_one_of(this.email_formats);
 	},
 
 	url: function() {
-		return this.domain;
+		return this.populate_one_of(this.url_formats);
 	},
 
 	ip: function() {
