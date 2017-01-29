066544ed24d94bde6e53915a941686cae78c716b
Muhammad Habib Rohman <mhrohman@live.com>
2017-01-29 12:10:57 +0700

    reformating id_ID locales

--- src/providers/id_ID/address.js
+++ src/providers/id_ID/address.js
@@ -1,7 +1,23 @@
 var provider = {
-	states: ['Aceh', 'Sumatera Utara', 'Sumatera Barat', 'Jambi', 'Bangka Belitung', 'Riau', 'Kepulauan Riau', 'Bengkulu', 'Sumatera Selatan', 'Lampung', 'Banten', 'DKI Jakarta', 'Jawa Barat', 'Jawa Tengah', 'Jawa Timur', 'Nusa Tenggara Timur', 'DI Yogyakarta', 'Bali', 'Nusa Tenggara Barat', 'Kalimantan Barat', 'Kalimantan Tengah', 'Kalimantan Selatan', 'Kalimantan Timur', 'Kalimantan Utara', 'Sulawesi Selatan', 'Sulawesi Utara', 'Gorontalo', 'Sulawesi Tengah', 'Sulawesi Barat', 'Sulawesi Tenggara', 'Maluku', 'Maluku Utara', 'Papua Barat', 'Papua'],
+	states: [
+		'Aceh', 'Sumatera Utara', 'Sumatera Barat', 'Jambi', 'Bangka Belitung',
+		'Riau', 'Kepulauan Riau', 'Bengkulu', 'Sumatera Selatan', 'Lampung', 'Banten',
+		'DKI Jakarta', 'Jawa Barat', 'Jawa Tengah', 'Jawa Timur', 'Nusa Tenggara Timur',
+		'DI Yogyakarta', 'Bali', 'Nusa Tenggara Barat', 'Kalimantan Barat', 'Kalimantan Tengah',
+		'Kalimantan Selatan', 'Kalimantan Timur', 'Kalimantan Utara', 'Sulawesi Selatan',
+		'Sulawesi Utara', 'Gorontalo', 'Sulawesi Tengah', 'Sulawesi Barat', 'Sulawesi Tenggara',
+		'Maluku', 'Maluku Utara', 'Papua Barat', 'Papua'
+	],
 
-	state_abbrs: ['Aceh', 'SumUt', 'SumBar', 'Jambi', 'BaBel', 'Riau', 'KepR', 'Bengkulu', 'SumSel', 'Lampung', 'Banten', 'DKI', 'JaBar', 'JaTeng', 'JaTim', 'NTT', 'DIY', 'Bali', 'NTB', 'KalBar', 'KalTeng', 'KalSel', 'KalTim', 'KalUt', 'SulSel', 'SulUt', 'Gorontalo', 'SulTeng', 'SulBar', 'SulTra', 'Maluku', 'MalUt', 'PapBar', 'Papua'],
+	state_abbrs: [
+		'Aceh', 'SumUt', 'SumBar', 'Jambi', 'BaBel',
+		'Riau', 'KepR', 'Bengkulu', 'SumSel', 'Lampung', 'Banten',
+		'DKI', 'JaBar', 'JaTeng', 'JaTim', 'NTT',
+		'DIY', 'Bali', 'NTB', 'KalBar', 'KalTeng',
+		'KalSel', 'KalTim', 'KalUt', 'SulSel',
+		'SulUt', 'Gorontalo', 'SulTeng','SulBar', 'SulTra',
+		'Maluku', 'MalUt', 'PapBar', 'Papua'
+	],
 
 	state: function() {
 		return this.random_element(this.states);
