19a3c8270b23bfce1cb3ac475e7698c103db808b
jorge.gonzález <dev@unapersona.com>
2022-03-08 12:56:53 +0100

    Adding es_ES locale

---
+++ src/providers/es_ES/color.js
@@ -0,0 +1,20 @@
+var provider = {
+    safe_color_names: [
+        'negro', 'blanco', 'verde', 'rojo', 'azul',
+        'gris', 'marrón', 'amarillo', 'rosa', 'violeta', 'naranja'
+    ],
+
+    color_names: [
+        'Rojo', 'Carmesí', 'Bermellón', 'Granate',
+        'Verde', 'Verde Esmeralda', 'Verde Oliva', 'Verde Militar',
+        'Azul', 'Azul Cobalto', 'Azul Marino', 'Añil', 'Índigo', 'Azul de Prusia',
+        'Magenta', 'Fucsia', 'Morado', 'Malva', 'Salmón', 'Rosa',
+        'Cián', 'Turquesa', 'Celeste', 'Aguamarina',
+        'Amarillo', 'Amarillo Limón', 'Ámbar', 'Topacio',
+        'Marrón', 'Caquí', 'Burdeos',
+        'Violeta', 'Lavanda', 'Púrpura',
+        'Beis', 'Blanco', 'Blanco Hueso', 'Blanco Marfil', 'Plata', 'Gris', 'Negro'
+    ]
+};
+
+module.exports = provider;
